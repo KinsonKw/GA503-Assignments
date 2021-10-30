46041a1bd6b4afce1ab89adfc629c20bb48d7e50
Kinson <kinsonkwong@lcad.edu>
2021-10-30 16:30:11 -0700

    Done with Assignment 1

--- Assets/Scripts/PryamidGenerator.cs
+++ Assets/Scripts/PryamidGenerator.cs
@@ -6,7 +6,9 @@ public class PryamidGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject prefab = null;
 
-
+    [SerializeField] private int row = 8;
+    [SerializeField] private int col = 8;
+    [SerializeField] private float moveBlock = .5F;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,21 @@ public class PryamidGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void BuildPyramid()
     {
- 
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                GameObject go = Instantiate(prefab, new Vector3(j + moveBlock, i, 0), Quaternion.identity);
+                go.SetActive(true);
+            }
+            col -= 1;
+            moveBlock += .5F;
+        }
     }
 }
+
